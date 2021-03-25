b34f4c273638f2f1c9f64328d53b35f2d1ec4e53
Ubuntu <ubuntu@ip-172-31-1-136.eu-west-1.compute.internal>
2021-03-25 10:41:57 +0000

    monitoreo

--- ejercicios/dia4/monitorizarPuerto.sh
+++ ejercicios/dia4/monitorizarPuerto.sh
@@ -9,3 +9,45 @@
 
 # Para chequear un puerto : NetCat: nc
 # nc -z host port
+
+function chequear_puerto(){
+    local _host=$1
+    local _puerto=$2
+    nc -z $_host $_puerto
+    echo $(( $? == 0 ))
+}
+
+function monitorizar_puerto(){
+    local _host=$1
+    local _puerto=$2
+    local _periodo_tiempo=$3
+    local _ICONOS=( ❌  ✅ )
+    local _hora=-1
+    local _fichero_monitoreo=monitoreo_$_host_$_puerto.log
+    
+    echo "" > $_fichero_monitoreo
+    
+    while true
+    do
+        local _esta_funcionando=$(chequear_puerto $_host $_puerto)
+        echo $_esta_funcionando
+        
+        if [[ "$(date +"%H:%M")" == "$_hora" ]]
+        then
+            ## No he cambiado de hora
+            echo -n ${_ICONOS[$_esta_funcionando]} >> $_fichero_monitoreo
+        else
+            ## He cambiado de hora
+            _hora=$(date +"%H:%M")
+            echo "">> $_fichero_monitoreo
+            echo -n $_hora: ${_ICONOS[$_esta_funcionando]} >> $_fichero_monitoreo
+        fi
+        [[ $_esta_funcionando == 0 ]] && echo $(date +"%d/%m/%y %H:%M") - $_host:$_puerto  >> errores.log
+        sleep $_periodo_tiempo
+    done
+}
+
+monitorizar_puerto localhost 8080 2
+
+
+
